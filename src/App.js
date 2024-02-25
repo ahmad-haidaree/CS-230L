88a998e7334d74bfcef907ef240ce064f72b3be6
ahmad-haidaree <abh00015@mix.wvu.edu>
2024-02-25 11:27:58 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section 02</h2>
+      <p>WVU ID: 800406079</p>
+      <p>Hi I am Ahmad Haidaree</p>
+
+    </div>
+  );
+}
+
+export default App;
