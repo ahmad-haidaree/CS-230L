abe83750f8b9580904d96b2a65bff17a7184e153
ahmad-haidaree <abh00015@mix.wvu.edu>
2024-03-02 12:02:03 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './Navbar';
+import Card from './Card';
+
+
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>      
       <h1>CS 230L</h1>
       <h2>Section 02</h2>
       <p>WVU ID: 800406079</p>
       <p>Hi I am Ahmad Haidaree</p>
+      <Card/>
 
     </div>
   );
---
+++ src/Card.js
@@ -0,0 +1,28 @@
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card 01</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card text-white bg-success mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card 02</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Card 03</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
